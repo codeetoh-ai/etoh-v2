61640d6e5896a0bcb305c190135fbcfa89fc6244
Amitansu-priyadarsan <amitansupriyadarsan70@gmail.com>
2026-03-26 15:14:11 +0530

    renaming

--- src/router.jsx
+++ src/router.jsx
@@ -2,39 +2,39 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 
 // About
-import OurMissionPage from './pages/about/our-mission'
-import HowWeWorkPage from './pages/about/how-we-work'
-import TeamPage from './pages/about/team'
-import CareersPage from './pages/about/careers'
+import OurMissionPage from './pages/about/our-mission/OurMission'
+import HowWeWorkPage from './pages/about/how-we-work/HowWeWork'
+import TeamPage from './pages/about/team/Team'
+import CareersPage from './pages/about/careers/Careers'
 
 // The System
-import TheSystemPage from './pages/the-system'
-import HospitalInfrastructurePage from './pages/the-system/hospital-infrastructure'
-import ForCliniciansPage from './pages/the-system/for-clinicians'
-import ForPatientsPage from './pages/the-system/for-patients'
-import EndToEndCarePage from './pages/the-system/end-to-end-care'
+import TheSystemPage from './pages/the-system/TheSystem'
+import HospitalInfrastructurePage from './pages/the-system/hospital-infrastructure/HospitalInfrastructure'
+import ForCliniciansPage from './pages/the-system/for-clinicians/ForClinicians'
+import ForPatientsPage from './pages/the-system/for-patients/ForPatients'
+import EndToEndCarePage from './pages/the-system/end-to-end-care/EndToEndCare'
 
 // Deployments
-import DeploymentsPage from './pages/deployments'
-import GovernmentHealthSystemsPage from './pages/deployments/government-health-systems'
-import PrivateHospitalNetworksPage from './pages/deployments/private-hospital-networks'
+import DeploymentsPage from './pages/deployments/Deployments'
+import GovernmentHealthSystemsPage from './pages/deployments/government-health-systems/GovernmentHealthSystems'
+import PrivateHospitalNetworksPage from './pages/deployments/private-hospital-networks/PrivateHospitalNetworks'
 
 // Research
-import ResearchPage from './pages/research'
-import ClinicalEvidencePage from './pages/research/clinical-evidence'
-import TechnologyPage from './pages/research/technology'
-import OutcomesIndexPage from './pages/research/outcomes-index'
+import ResearchPage from './pages/research/Research'
+import ClinicalEvidencePage from './pages/research/clinical-evidence/ClinicalEvidence'
+import TechnologyPage from './pages/research/technology/Technology'
+import OutcomesIndexPage from './pages/research/outcomes-index/OutcomesIndex'
 
 // Impact
-import ImpactPage from './pages/impact'
-import PatientOutcomesPage from './pages/impact/patient-outcomes'
-import HealthcareAccessPage from './pages/impact/healthcare-access'
-import PublicHealthMissionPage from './pages/impact/public-health-mission'
+import ImpactPage from './pages/impact/Impact'
+import PatientOutcomesPage from './pages/impact/patient-outcomes/PatientOutcomes'
+import HealthcareAccessPage from './pages/impact/healthcare-access/HealthcareAccess'
+import PublicHealthMissionPage from './pages/impact/public-health-mission/PublicHealthMission'
 
 // News & Insights
-import NewsInsightsPage from './pages/news-insights'
-import NewsPage from './pages/news-insights/news'
-import InsightsPage from './pages/news-insights/insights'
+import NewsInsightsPage from './pages/news-insights/NewsInsights'
+import NewsPage from './pages/news-insights/news/News'
+import InsightsPage from './pages/news-insights/insights/Insights'
 
 export const router = createBrowserRouter([
     { path: '/', element: <App /> },
